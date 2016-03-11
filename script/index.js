462dcfd5f8458860c66352a8481aeb025555bf8d
sharpmental <shunmin.wu@gmail.com>
2016-03-11 10:55:49 +0800

    update

--- script/index.js
+++ script/index.js
@@ -1,16 +1,71 @@
 var counter = 0;
 
 $("document").ready(function () {
-    $("#startdate").datepicker({
+    $(".startdate").datepicker({
         altFormat: "yy-mm-dd"
     });
-    $("#enddate").datepicker({
+    $(".enddate").datepicker({
+        altFormat: "yy-mm-dd"
+    });
+    $("#s_date").datepicker({
+        altFormat: "yy-mm-dd"
+    });
+    $("#e_date").datepicker({
         altFormat: "yy-mm-dd"
     });
 
+    $("#btncalc").click(function(event){
+        var g_startdate;
+        var g_enddate;
+        
+        var array_date=new Array();
+        //calculate start date, end date, and generate the array
+        var s_d1= $("#s_date").text;
+        var e_d1 = $("#e_date").text;
+        
+        
+        //for each in line, get the data
+        //if the cycle is day
+            //get the start date
+            //for each in array_date
+                //if the date is > this.startdate, and < this.enddate
+                //put the spent into every day into array_date;
+        //else if the cycle is week
+            //get the this.startdate and its weekday
+            //get the this.weekcycle
+            //for from g_startdate-itsweekday + this.startdate.weekday
+                //if the date < this.startdate, then add 7 day,count the weekcycle
+                //else if weekcycle < this.weekcycle, add 7 day, coun the weekcycle
+                //else if date > this.enddate
+                //end loop
+                //else
+                //put the spent into array_date, reset the weekcycle
+        //else if the cycle is month
+            //get the this.startdate and its monthday
+            //get the this.monthcycle
+            //for from g_startdate-its.monthday + this.startdate.monthday
+                //if the date < this.startdate, then add 7 day,count the monthcycle
+                //else if monthcycle < this.monthcycle, add 7 day, coun the monthcycle
+                //else if date > this.enddate
+                //end loop
+                //else
+                //put the spent into array_date, reset the monthcycle
+        
+        //else if the cycle is year
+        
+        //else skiperror
+        
+    });
+    
+    $("#btndraw").click(function(event){
+    });
+    
+    $("#btnsave").click(function(event){
+    });
+    
     $("#btnaddline").click(function () {
         counter = counter + 1;
-        var item = '<div class=\"row\" id=\"list-'+counter+'\">';
+        var item = '<div class=\"row\" id=\"list-' + counter + '\">';
         item = item + '<div class=\"form-inline\">';
         item = item + '<div class=\"form-group\">';
         item = item + '<label>数量</label>';
@@ -38,17 +93,27 @@ $("document").ready(function () {
         item = item + '</div>';
         item = item + '<div class=\"form-group\">';
         item = item + '<label>起始日期</label>';
-        item = item + '<input type=\"text\" class=\"form-control input-sm\" id=\"startdate\" placeholder=\"2011-01-01\">';
+        item = item + '<input type=\"text\" class=\"form-control input-sm startdate\" id=\"\" placeholder=\"2011-01-01\">';
         item = item + '</div>';
         item = item + '<div class=\"form-group\">';
         item = item + '<label>结束日期</label>';
-        item = item + '<input type=\"text\" class=\"form-control input-sm\" id=\"enddate\" placeholder=\"2011-01-01\">';
+        item = item + '<input type=\"text\" class=\"form-control input-sm enddate\" id=\"\" placeholder=\"2011-01-01\">';
         item = item + '</div>'
-        item = item + '<div class=\"btn btn-default\" id=\"btn-delline-' + counter + '\">删除此项</div>';
+        item = item + '<div class=\"btn btn-default delline\" id=\"btn-delline-' + counter + '\">删除此项</div>';
 
         item = item + '</div></div>';
 
         var i = counter - 1;
         $("#list-" + i).after(item);
+        $(".startdate").datepicker({
+            altFormat: "yy-mm-dd"
+        });
+        $(".enddate").datepicker({
+            altFormat: "yy-mm-dd"
+        });
+        $(".delline").click(function(event){
+            var id = event.currentTarget.parentElement.parentElement.getAttribute("id");
+            id = id+"111";
+        })
     });
 });
